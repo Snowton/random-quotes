e7de35a25cc56bb917eb64e54fa07cb79d123f8f
Shreya Mogulothu <shreya.mogulothu@gmail.com>
2020-05-08 11:16:26 -0400

    Updates

---
+++ precache-manifest.07bfc413ab85c9a554e684c72b7d4a09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593482af58c98b513e1e6cf2147d9b90",
+    "url": "/random-quotes/index.html"
+  },
+  {
+    "revision": "ef2b57d36917746e6b51",
+    "url": "/random-quotes/static/js/2.1415d568.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quotes/static/js/2.1415d568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "202bc5081ce1fd81eb45",
+    "url": "/random-quotes/static/js/main.40e868ae.chunk.js"
+  },
+  {
+    "revision": "2d467ecaf7e5bd79de03",
+    "url": "/random-quotes/static/js/runtime-main.3c28e3bb.js"
+  }
+]);
