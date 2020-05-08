f38c3831a623907fc42b099751d01dd8ccd41eef
Shreya Mogulothu <shreya.mogulothu@gmail.com>
2020-05-08 11:13:47 -0400

    Updates

---
+++ precache-manifest.72bf4f4f0116d0cd9dab675c03435f9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e9ad2c6e0d39cd2b283698411d82e5",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ef2b57d36917746e6b51",
+    "url": "/my-app/static/js/2.1415d568.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.1415d568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "202bc5081ce1fd81eb45",
+    "url": "/my-app/static/js/main.40e868ae.chunk.js"
+  },
+  {
+    "revision": "85a2f30ada6950cff405",
+    "url": "/my-app/static/js/runtime-main.d8fbec38.js"
+  }
+]);
